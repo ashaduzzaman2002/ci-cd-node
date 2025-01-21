442359ef4f57070254acef37ab837b10c94fa697
Ashadu Zaman <168162401+ashadu-workmates@users.noreply.github.com>
2025-01-22 01:26:52 +0530

    update permission

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ import express from 'express'
 const app = express()
 
 app.get('/', (req, res) => res.send('Hello World'))
-app.get('/health', (req, res) => res.send('Hello Health'))
+app.get('/health', (req, res) => res.send('Hello World'))
 
 app.listen(8000, () => console.log('Server is running on port 8000'))
