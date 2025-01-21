1b157b60414c49e18b48525f8ad6b9263f3b3744
Ashadu Zaman <168162401+ashadu-workmates@users.noreply.github.com>
2025-01-22 00:55:07 +0530

    add new route

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ import express from 'express'
 const app = express()
 
 app.get('/', (req, res) => res.send('Hello Server'))
+app.get('/health', (req, res) => res.send('Hello Health'))
 
 app.listen(8000, () => console.log('Server is running on port 8000'))
