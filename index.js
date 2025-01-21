583cf54a86ebdf72e35075391f4c3b724fef466c
Ashadu Zaman <168162401+ashadu-workmates@users.noreply.github.com>
2025-01-21 23:33:41 +0530

    init

---
+++ index.js
@@ -0,0 +1,7 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello Server'))
+
+app.listen(8000, () => console.log('Server is running on port 8000'))
