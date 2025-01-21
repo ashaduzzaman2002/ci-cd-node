e2b3b148d936a290319423c37bc33b6185382762
Ashadu Zaman <168162401+ashadu-workmates@users.noreply.github.com>
2025-01-22 01:30:27 +0530

    test

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ import express from 'express'
 const app = express()
 
 app.get('/', (req, res) => res.send('Hello World'))
-app.get('/health', (req, res) => res.send('Hello World'))
+app.get('/health', (req, res) => res.send('Hello'))
 
 app.listen(8000, () => console.log('Server is running on port 8000'))
